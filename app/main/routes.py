0f5ea899e44d450c79115530e7ad11631bc15e22
Archamat <gorkempoyraz@hotmail.com>
2025-02-12 00:15:57 +0300

    feat: applicant list button added and created projects part updated

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
 from app.projects.models import Project
+from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     projects = Project.query.order_by(Project.id.desc()).all()
-    return render_template('home.html', current_page='home', projects=projects)
+    return render_template('home.html', current_page='home', projects=projects,current_user=current_user)
 
 
