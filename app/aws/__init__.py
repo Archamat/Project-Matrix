6b8bcc318da956e5baa15c36b34e7d1da507391f
burakvarol123 <burakvarol123@github.com>
2025-09-08 11:59:33 +0100

    created aws implementation with helper functions to upload pp

---
+++ app/aws/__init__.py
@@ -0,0 +1,9 @@
+from .s3 import s3_client, s3_resource, s3_bucket, upload_fileobj_private, object_url
+
+__all__ = [
+    "s3_client",
+    "s3_resource",
+    "s3_bucket",
+    "upload_fileobj_private",
+    "object_url",
+]
